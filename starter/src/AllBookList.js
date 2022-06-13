44a8f7c364736cc55f152f84adc67b9e5c6b5d69
Leandro Meili <leandro.meili@gmail.com>
2022-06-12 23:57:11 -0300

    feat: Implement book movement between shelfs and Routing
    Refactors the original starter project into react components
    
    Add routing
    - "/" for the main page
    - "/search" for the search page
    
    Books dynamically move between shelves as the user click and choose the selector component.

---
+++ starter/src/AllBookList.js
@@ -0,0 +1,37 @@
+import {Link} from "react-router-dom";
+import ShelfList from "./ShelfList";
+import {useState} from "react";
+
+
+
+const AllBookList = ({bookCollection, onShelfChange}) => {
+    const currentShelf = bookCollection.filter((item) =>
+        item.shelf === "currentlyReading"
+    );
+    const wantShelf = bookCollection.filter((item) =>
+        item.shelf === "wantToRead"
+    );
+    const readShelf = bookCollection.filter((item) =>
+        item.shelf === "read"
+    );
+
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <ShelfList shelfName={"Currently Reading"} books={currentShelf} onShelfChange={onShelfChange}/>
+                    <ShelfList shelfName={"Want to Read"} books={wantShelf} onShelfChange={onShelfChange}/>
+                    <ShelfList shelfName={"Read"} books={readShelf} onShelfChange={onShelfChange}/>
+                </div>
+            </div>
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
+            </div>
+        </div>
+    )
+}
+
+export default AllBookList
---
+++ starter/src/Book.js
@@ -0,0 +1,35 @@
+import {useState} from "react";
+
+const ShelfSelector = ({metadata, handle}) => {
+
+    return (
+        <div className="book-shelf-changer">
+            <select value={metadata.shelf} onChange={(event) => handle(event, metadata.bookname)}>
+                <option value="none" disabled> Move to...</option>
+                <option value="currentlyReading"> Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
+        </div>
+    )
+}
+
+const Book = ({metadata, onShelfChange}) => {
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div
+                    className="book-cover"
+                    style={metadata.style}
+                ></div>
+                <ShelfSelector metadata={metadata} handle={onShelfChange}/>
+            </div>
+            <div className="book-title">{metadata.bookname}</div>
+            <div className="book-authors">{metadata.author}</div>
+        </div>
+    )
+}
+
+export default Book
